bbcb28f9b336686b1524792eb380a8f0790d2680
dobredani <dobre.dani.gtalk@gmail.com>
2020-04-04 00:30:45 +0300

    in progress: print mulitple markers on page

--- main.js
+++ main.js
@@ -33,13 +33,14 @@ function generateMarkerSvg(width, height, bits) {
 }
 
 var dict;
+var layouts;
 
 function generateArucoMarker(width, height, dictName, id) {
 	console.log('Generate ArUco marker ' + dictName + ' ' + id);
 
 	var bytes = dict[dictName][id];
 	var bits = [];
-	var bitsCount = width *  height;
+	var bitsCount = width * height;
 
 	// Parse marker's bytes
 	for (var byte of bytes) {
@@ -52,11 +53,15 @@ function generateArucoMarker(width, height, dictName, id) {
 	return generateMarkerSvg(width, height, bits);
 }
 
-var loadDict = $.getJSON('dict.json', function(data) {
+var loadDict = $.getJSON('dict.json', function (data) {
 	dict = data;
 });
 
-$(function() {
+var loadLayouts = $.getJSON('layouts.json', function (data) {
+	layouts = data;
+});
+
+$(function () {
 	var dictSelect = $('.setup select[name=dict]');
 	var markerIdInput = $('.setup input[name=id]');
 	var sizeInput = $('.setup input[name=size]');
@@ -69,7 +74,7 @@ $(function() {
 		var height = Number(dictSelect.find('option:selected').attr('data-height'));
 
 		// Wait until dict data is loaded
-		loadDict.then(function() {
+		loadDict.then(function () {
 			// Generate marker
 			var svg = generateArucoMarker(width, height, dictName, markerId, size);
 			svg.attr({
@@ -89,4 +94,53 @@ $(function() {
 
 	dictSelect.change(updateMarker);
 	$('.setup input').on('input', updateMarker);
+
+});
+
+function positionMarker(realMarker, realParent, jsonMarker, jsonParent){
+	// var style = getComputedStyle(realParent);
+	var left = jsonParent.margin + jsonMarker.left/(jsonParent.width - 2* jsonParent.margin) * 21.0; //* style.width;
+	var top = jsonParent.margin + jsonMarker.top/(jsonParent.height - 2* jsonParent.margin) * 29.7; //* style.width;
+	var width = jsonMarker.size/jsonParent.width * 21.0; //* style.width;
+	var height = jsonMarker.size/jsonParent.width * 21.0; //* style.height;
+
+	realMarker.setAttribute('style','width:' + width + 'mm;' + ' height:' + height + 'mm;' + ' top:' + top + 'mm;' + ' left:' + left + 'mm;' );
+};
+
+$(document).ready(function showPrintLayouts() {
+
+	// Wait until dict data is loaded
+	loadLayouts.then(function () {
+		$.each (layouts, function(i, layout){
+
+			// create a div element in the left panel for each print layout
+			var element = document.createElement("div");
+			element.setAttribute('class', 'print-layout');
+			element.setAttribute('id', i);
+			element.addEventListener("click", function(){
+				var coll = document.getElementsByClassName('print-layout');
+
+				$.each(coll, function(i, elem){elem.setAttribute('class','print-layout');})
+				this.setAttribute('class', 'print-layout selected');
+
+			});
+
+			$.each(layout.markers, function(i, marker){
+				var markerPreview = document.createElement("div");
+				markerPreview.setAttribute('class','marker-preview');
+				positionMarker(markerPreview, element, marker,layout);
+				
+				element.appendChild(markerPreview);
+			});
+
+			if (element != null) {
+				document.getElementById('print-layouts').appendChild(element);
+			}
+		});
+
+		// select default page layout
+		// layouts[0].setAttribute('class', 'print-layout selected')
+	});
+
+
 });
